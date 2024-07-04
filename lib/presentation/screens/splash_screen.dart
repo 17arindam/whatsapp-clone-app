45e1dfd2f9ca6bfe56109162a9babeb9c13f1449
Arindam <17arindambera@gmail.com>
2024-07-04 13:53:40 +0530

    feat:splash screen added

---
+++ lib/presentation/screens/splash_screen.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/presentation/screens/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3), () { 
+      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>HomeScreen()),(route)=>false);
+    });
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.black,body: Center(child: Text('WhatsApp Clone',style: TextStyle(fontSize: 20,color: Colors.white),),),);
+  }
+}
