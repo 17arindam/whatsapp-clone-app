1b315e092469cdfd41fb9c1d8b219ae1bc6e27f6
Arindam <17arindambera@gmail.com>
2024-07-04 17:23:23 +0530

    feat:country picker added

---
+++ lib/presentation/screens/registration_screen.dart
@@ -0,0 +1,125 @@
+import 'package:country_pickers/country.dart';
+import 'package:country_pickers/country_pickers.dart';
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/presentation/widgets/theme/style.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({super.key});
+
+  @override
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  Country _selectedFilteredDialogCountry =
+      CountryPickerUtils.getCountryByPhoneCode('91');
+  String _countryCode = '+91';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(''),
+                Text(
+                  'Verify your phone number',
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: greenColor,
+                      fontWeight: FontWeight.w500),
+                ),
+                Icon(Icons.more_vert),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "WhatsApp Clone will send an SMS message (carrier charges may apply) to verify your phone number. Enter your country code and phone number:",
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            ListTile(
+              onTap: _openFilteredCountryPickerDialog,
+              title: _buildDialogItem(_selectedFilteredDialogCountry),
+            ),
+            // Add your TextField for phone number input here
+            // TextField(
+            //   keyboardType: TextInputType.phone,
+            //   decoration: InputDecoration(
+            //     prefixText: _countryCode,
+            //     hintText: 'Phone number',
+            //   ),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _openFilteredCountryPickerDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => Theme(
+        data: Theme.of(context).copyWith(primaryColor: primaryColor),
+        child: CountryPickerDialog(
+          onValuePicked: (Country country) {
+            setState(() {
+              _selectedFilteredDialogCountry = country;
+              _countryCode = '+${country.phoneCode}';
+            });
+          },
+          titlePadding: EdgeInsets.all(8),
+          searchCursorColor: Colors.black,
+          searchInputDecoration: InputDecoration(hintText: 'Search'),
+          isSearchable: true,
+          title: Text('Select Your Phone Code'),
+          itemBuilder: _buildDialogItem,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDialogItem(Country country) {
+    return Container(
+      height: 40,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: greenColor, width: 1),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          CountryPickerUtils.getDefaultFlagImage(country),
+          const SizedBox(width: 8),
+          Text(
+            '+${country.phoneCode}',
+            style: TextStyle(overflow: TextOverflow.ellipsis),
+          ),
+          const SizedBox(width: 8),
+          Expanded(
+            child: Text(
+              country.name,
+              //overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+              //style: TextStyle(overflow: TextOverflow.ellipsis),
+            ),
+          ),
+          Spacer(),
+          Icon(Icons.arrow_drop_down),
+        ],
+      ),
+    );
+  }
+}
--- lib/presentation/screens/splash_screen.dart
+++ lib/presentation/screens/splash_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/presentation/screens/home_screen.dart';
+import 'package:whatsapp_clone/presentation/screens/registration_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -13,13 +14,25 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    Timer(Duration(seconds: 3), () { 
-      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>HomeScreen()),(route)=>false);
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => RegistrationScreen()),
+          (route) => false);
     });
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.black,body: Center(child: Text('WhatsApp Clone',style: TextStyle(fontSize: 20,color: Colors.white),),),);
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Text(
+          'WhatsApp Clone',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+      ),
+    );
   }
 }
