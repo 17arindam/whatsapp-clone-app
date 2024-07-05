37587ba6392b116c845116f78bcddabeaeb75096
Arindam <17arindambera@gmail.com>
2024-07-05 20:37:21 +0530

    feat:chat page and status page updated

--- lib/presentation/pages/chat_page.dart
+++ lib/presentation/pages/chat_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/presentation/pages/subpages/single_item_chat_user_page.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Chat Page'),),);
+    return Scaffold(
+      body: Column(children: [Expanded(child: ListView.builder(itemCount: 10,itemBuilder: (_,index){
+        return SingleItemChatUserPage( name: 'arindam',recentSendMessage: 'urgent',time: '12:47 AM',);
+      }))],),
+      floatingActionButton: FloatingActionButton(onPressed: (){},child: Icon(Icons.chat),),
+    );
   }
 }
